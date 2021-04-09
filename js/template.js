2bc2ab29d7b33dc71744ce4b0cb2c24737ed25a7
kento <fujiken20c@gmail.com>
2021-04-09 15:57:05 +0900

    if文を追加し無限ループを制御

--- js/template.js
+++ js/template.js
@@ -1,7 +1,12 @@
+const a = 0
+
 function header(){
-    var html = "";
-    html += '<header style="background-color: syan;">';
-    html += '<p>自己紹介ウェブサイト</p>';
-    html += '</header>';
-    document.write(html);
+    if(a++ === 0)
+        {
+        var html = "";
+        html += '<header style="background-color: syan;">';
+        html += '<p>自己紹介ウェブサイト</p>';
+        html += '</header>';
+        document.write(html);
+        }
     }
