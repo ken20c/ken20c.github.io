1e4226f8949b2ea2a9669355e5d0658bf704dc3d
kento <fujiken20c@gmail.com>
2021-04-09 15:51:34 +0900

    スタイルを追加

--- js/template.js
+++ js/template.js
@@ -1,7 +1,7 @@
 function header(){
     var html = "";
-    html += '<header>';
-    html += '<p>自己紹介ウェブサイト</small></p>';
+    html += '<header style="background-color: syan;">';
+    html += '<p>自己紹介ウェブサイト</p>';
     html += '</header>';
     document.write(html);
     console.log(`header()は${header()}です。`);
